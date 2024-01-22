ca5fcc6dad94dc4c9762d49f6a78beccb0559c4a
jamessandersonn <jamesandersonlink@gmail.com>
2024-01-22 15:04:48 +1100

    a

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ function showphotos() {
   $("#photos_container").addClass("animated slideInDown");
   setTimeout(function () {
     $("#photos_container").removeClass("animated slideInDown");
-  }, 1000);
+  }, 500);
 }
 function closephotos() {
   $("#photos_container").addClass("animated slideOutUp");
   setTimeout(function () {
     $("#photos_container").removeClass("animated slideOutUp");
     $("#photos_container").css("display", "none");
-  }, 1000);
+  }, 500);
 }
 function showabout() {
   $("#about_container").css("display", "inherit");
   $("#about_container").addClass("animated slideInLeft");
   setTimeout(function () {
     $("#about_container").removeClass("animated slideInLeft");
-  }, 1000);
+  }, 500);
 }
 function closeabout() {
   $("#about_container").addClass("animated slideOutLeft");
   setTimeout(function () {
     $("#about_container").removeClass("animated slideOutLeft");
     $("#about_container").css("display", "none");
-  }, 1000);
+  }, 500);
 }
 
 function showcontact() {
@@ -32,14 +32,14 @@ function showcontact() {
   $("#contact_container").addClass("animated slideInRight");
   setTimeout(function () {
     $("#contact_container").removeClass("animated slideInRight");
-  }, 1000);
+  }, 500);
 }
 function closecontact() {
   $("#contact_container").addClass("animated slideOutRight");
   setTimeout(function () {
     $("#contact_container").removeClass("animated slideOutRight");
     $("#contact_container").css("display", "none");
-  }, 1000);
+  }, 500);
 }
 setTimeout(function () {
   $("#loading").addClass("animated fadeOut");
@@ -50,5 +50,5 @@ setTimeout(function () {
     $("#about").removeClass("animated fadeIn");
     $("#contact").removeClass("animated fadeIn");
     $("#photos").removeClass("animated fadeIn");
-  }, 1000);
-}, 1500);
+  }, 500);
+}, 1000);
